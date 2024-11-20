b42c6edab9704260281f0aa2d629aca2de46b0cf
dylan klages <dylan.klages@ndsu.edu>
2024-11-20 14:44:16 -0600

    New Class - Student.cs

---
+++ Components/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Klages Nov 20.Components.Models
+{
+    public class Student
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastDescription { get; set; }
+}
+}
